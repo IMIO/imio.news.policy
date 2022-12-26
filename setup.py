5edce1c7fa84a726ed0c4746b933fe8e8d808dc9
Laurent Lasudry <lasudry@gmail.com>
2022-12-26 10:16:28 +0100

    Remove z3c.autoinclude

--- setup.py
+++ setup.py
@@ -77,8 +77,5 @@ setup(
             "plone.app.robotframework[debug]",
         ],
     },
-    entry_points="""
-    [z3c.autoinclude.plugin]
-    target = plone
-    """,
+    entry_points="""""",
 )
